23e7499ca6803a73ebfde224fdb59c9618afa791
Moritz Hepp <moritz.hepp@gmx.de>
2018-11-28 17:20:22 +0100

    Added Maze Service

---
+++ src/app/maze.service.ts
@@ -0,0 +1,137 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Cell {
+  walls: [boolean, boolean, boolean, boolean];
+  visited: boolean;
+}
+
+export enum Direction {
+  NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3
+}
+
+export const Directions = [
+  0, 1, 2, 3
+];
+
+interface Coordinates {
+  x: number;
+  y: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MazeService {
+
+  constructor() { }
+
+  generateMaze(width: number, height: number): Observable<Cell & Coordinates> {
+    return new Observable(observer => {
+      const maze = new Array<Array<Cell>>();
+    const indexQueue = this.getQueue(width, height);
+    const stack = new Array<Coordinates>();
+    for (let i = 0; i < width; i++) {
+      maze.push(new Array<Cell>());
+      for (let j = 0; j < height; j++) {
+        maze[i].push({
+          walls: [true, true, true, true],
+          visited: false
+        });
+      }
+    }
+
+    let currentIndex: Coordinates;
+    let current: Cell;
+    do {
+      currentIndex = indexQueue.pop();
+
+      if (currentIndex === undefined) {
+        console.log('Got back undefined of Queue');
+      }
+
+      current = maze[currentIndex.x][currentIndex.y];
+
+      current.visited = true;
+
+      // Check if unvisited neighbours are present
+      const unvisited = new Array<Cell & { dir: number, cord: Coordinates }>();
+      // iterate over north, east, south, west
+      for (const direction of Directions) {
+        const coordinates: Coordinates = { x: currentIndex.x, y: currentIndex.y };
+        if (direction === Direction.NORTH) {
+          coordinates.y -= 1;
+        } else if (direction === Direction.SOUTH) {
+          coordinates.y += 1;
+        } else if (direction === Direction.EAST) {
+          coordinates.x += 1;
+        } else if (direction === Direction.WEST) {
+          coordinates.x -= 1;
+        } else {
+          console.log('Error while processign coordinates, got direction: ' + direction);
+          return;
+        }
+
+        // Check if out of bounds -> if yes, skip this direction
+        if (coordinates.x < 0 || coordinates.x >= width || coordinates.y < 0 || coordinates.y >= height) {
+          continue;
+        }
+
+        // Check if neighbour has been visited yet
+        const neighbour = maze[coordinates.x][coordinates.y];
+        if (!neighbour.visited) {
+          unvisited.push({ ...neighbour, dir: direction, cord: coordinates });
+        }
+      }
+
+      // If some unvisited choose, randomly one of them to visit
+      if (unvisited.length > 0) {
+        stack.push(currentIndex);
+
+        const randomIndex = this.getRandom(0, unvisited.length - 1);
+        // Remove wall between current and next cell
+        const next = unvisited[randomIndex];
+
+        // Remove the wall
+        current.walls[next.dir] = false;
+        next.walls[(next.dir + 2) % 4] = false;
+        // Make next to to current in next round
+        indexQueue.push(next.cord);
+
+        observer.next({...current, x: currentIndex.x, y: currentIndex.y});
+        observer.next({...next, x: next.cord.x, y: next.cord.y});
+      } else {
+        const next = stack.pop();
+
+        if (next !== undefined) {
+          indexQueue.push(next);
+        }
+      }
+    } while (indexQueue.length > 0);
+    observer.complete();
+    });
+  }
+
+  getQueue(x: number, y: number): Array<Coordinates> {
+    const n = x * y;
+    const array = new Array<Coordinates>();
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y; j++) {
+        array.push({ x: i, y: j });
+      }
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      // Random to swap
+      const randomChoiceIndex = this.getRandom(i, array.length - 1);
+      // Swapping
+      [array[i], array[randomChoiceIndex]] = [array[randomChoiceIndex], array[i]];
+    }
+
+    return array;
+  }
+
+  getRandom(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
--- src/app/maze/maze.component.ts
+++ src/app/maze/maze.component.ts
@@ -1,28 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnChanges } from '@angular/core';
 import { range } from 'rxjs';
 import { CastExpr } from '@angular/compiler';
 import { createText } from '@angular/core/src/view/text';
 import { element } from '@angular/core/src/render3';
-
-interface Cell {
-  walls: [boolean, boolean, boolean, boolean];
-  visited: boolean;
-}
-
-interface Coordinates {
-  x: number;
-  y: number;
-}
+import { Cell, MazeService } from '../maze.service';
 
 const entrySize = 50;
-
-enum Direction {
-  NORTH= 0, EAST= 1, SOUTH= 2, WEST= 3
-}
-
-const Directions = [
-  0, 1, 2, 3
-];
 
 @Component({
   selector: 'app-maze',
@@ -32,106 +15,35 @@ const Directions = [
 export class MazeComponent implements OnInit {
 
   @ViewChild('maze') canvas: ElementRef;
+  canvasSize: number;
 
-  constructor() { }
+  constructor(
+    private service: MazeService
+  ) {
+    this.drawCell = this.drawCell.bind(this);
+  }
+
+  updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    this.canvasSize = Math.floor(width < height ? width : height);
+    this.canvasSize = this.canvasSize - (this.canvasSize % entrySize);
+  }
 
   ngOnInit() {
-    /*
-    A "true" entry inside the maze means, the border is present
-    */
+    // Get available screensize
+    this.updateSize();
 
-    const x = this.canvas.nativeElement.width / entrySize;
-    const y = this.canvas.nativeElement.height / entrySize;
-
-    const maze = new Array<Array<Cell>>();
-    const indexQueue = this.getQueue(x, y);
-    const stack = new Array<Coordinates>();
-    for (let i = 0; i < x; i++) {
-      maze.push(new Array<Cell>());
-      for (let j = 0; j < y; j++) {
-        maze[i].push({
-          walls: [true, true, true, true],
-          visited: false
-        });
-      }
-    }
-
-    let currentIndex: Coordinates;
-    let current: Cell;
-    do {
-      currentIndex = indexQueue.pop();
-
-      if (currentIndex === undefined) {
-        console.log('Got back undefined of Queue');
-      }
-
-      current = maze[currentIndex.x][currentIndex.y];
-
-      current.visited = true;
-
-      // Check if unvisited neighbours are present
-      const unvisited = new Array<Cell & {dir: number, cord: Coordinates}>();
-      // iterate over north, east, south, west
-      for (const direction of Directions) {
-        const coordinates: Coordinates = {x: currentIndex.x, y: currentIndex.y};
-        if (direction === Direction.NORTH) {
-          coordinates.y -= 1;
-        } else if (direction === Direction.SOUTH) {
-          coordinates.y += 1;
-        } else if (direction === Direction.EAST) {
-          coordinates.x += 1;
-        } else if (direction === Direction.WEST) {
-          coordinates.x -= 1;
-        } else {
-          console.log('Error while processign coordinates, got direction: ' + direction);
-          return;
-        }
-
-        // Check if out of bounds -> if yes, skip this direction
-        if (coordinates.x < 0 || coordinates.x >= x || coordinates.y < 0 || coordinates.y >= y) {
-          continue;
-        }
-
-        // Check if neighbour has been visited yet
-        const neighbour = maze[coordinates.x][coordinates.y];
-        if (!neighbour.visited) {
-          unvisited.push({...neighbour, dir: direction, cord: coordinates});
-        }
-      }
-
-      // If some unvisited choose, randomly one of them to visit
-      if (unvisited.length > 0) {
-        stack.push(currentIndex);
-
-        const randomIndex = this.getRandom(0, unvisited.length - 1);
-        // Remove wall between current and next cell
-        const next = unvisited[randomIndex];
-
-        // Remove the wall
-        current.walls[next.dir] = false;
-        next.walls[(next.dir + 2) % 4] = false;
-        // Make next to to current in next round
-        indexQueue.push(next.cord);
-
-        this.drawCell(currentIndex.x, currentIndex.y, current);
-        this.drawCell(next.cord.x, next.cord.y, next);
-      } else {
-        const next = stack.pop();
-
-        if (next !== undefined) {
-          indexQueue.push(next);
-        }
-      }
-    } while (indexQueue.length > 0);
-
-    // Draw cells
-    /*
-    for (let i = 0; i < x; i++) {
-      for (let j = 0; j < y; j++) {
-        this.drawCell(i, j, maze[i][j]);
-      }
-    }
-    */
+    this.service.generateMaze(this.canvasSize, this.canvasSize)
+      .subscribe({
+        next: cellCord => {
+          console.log(`Drawing Cell @ x: ${cellCord.x}, y: ${cellCord.y}`);
+          this.drawCell(cellCord.x, cellCord.y, cellCord);
+        },
+        complete: () => console.log('Finished generating maze!')
+      });
+    // setTimeout(this.redraw, 1000);
   }
 
   drawCell(x_: number, y_: number, cell: Cell) {
@@ -170,33 +82,9 @@ export class MazeComponent implements OnInit {
       ctx.fillStyle = '#ffffff';
       ctx.fillRect(x, y, 1, entrySize);
     }
-    ctx.fill();
 
     // Fill the rect
     ctx.fillStyle = '#ffffff';
     ctx.fillRect(x + 1, y + 1, entrySize - 2, entrySize - 2);
-  }
-
-  getQueue(x: number, y: number): Array<Coordinates> {
-    const n = x * y;
-    const array = new Array<Coordinates>();
-    for (let i = 0; i < x; i++) {
-      for (let j = 0; j < y; j++) {
-        array.push({x: i, y: j});
-      }
-    }
-
-    for (let i = 0; i < array.length; i++) {
-      // Random to swap
-      const randomChoiceIndex = this.getRandom(i, array.length - 1);
-      // Swapping
-      [array[i], array[randomChoiceIndex]] = [array[randomChoiceIndex], array[i]];
-    }
-
-    return array;
-  }
-
-  getRandom(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
